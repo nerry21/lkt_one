87e313c57e698f85d63cd80934cb5a982d23f61f
nerry21 <nerrypopindo@gmail.com>
2026-03-12 23:34:58 +0700

    tambah

--- resources/views/layouts/partials/vite-assets.blade.php
+++ resources/views/layouts/partials/vite-assets.blade.php
@@ -1,32 +1,22 @@
 @php
-    $viteManifestPath = public_path('build/manifest.json');
+    $viteManifestPath = base_path('../build/manifest.json');
     $viteManifest = file_exists($viteManifestPath)
         ? json_decode(file_get_contents($viteManifestPath), true)
         : null;
 
-    $requestHost = request()->getHost();
-    $isLocalHost = in_array($requestHost, ['localhost', '127.0.0.1', '::1'], true)
-        || str_ends_with($requestHost, '.test');
-    $shouldUseHotReload = app()->environment('local')
-        && file_exists(public_path('hot'))
-        && $isLocalHost;
+    $cssAsset = is_array($viteManifest)
+        ? ($viteManifest['resources/css/app.css']['file'] ?? null)
+        : null;
+
+    $jsAsset = is_array($viteManifest)
+        ? ($viteManifest['resources/js/app.js']['file'] ?? null)
+        : null;
 @endphp
 
-@if ($shouldUseHotReload)
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-@elseif (is_array($viteManifest))
-    @php
-        $cssAsset = $viteManifest['resources/css/app.css']['file'] ?? null;
-        $jsAsset = $viteManifest['resources/js/app.js']['file'] ?? null;
-    @endphp
+@if (is_string($cssAsset) && $cssAsset !== '')
+    <link rel="stylesheet" href="{{ asset('build/' . ltrim($cssAsset, '/')) }}">
+@endif
 
-    @if (is_string($cssAsset) && $cssAsset !== '')
-        <link rel="stylesheet" href="{{ '/build/' . ltrim($cssAsset, '/') }}">
-    @endif
-
-    @if (is_string($jsAsset) && $jsAsset !== '')
-        <script type="module" src="{{ '/build/' . ltrim($jsAsset, '/') }}"></script>
-    @endif
-@else
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@if (is_string($jsAsset) && $jsAsset !== '')
+    <script type="module" src="{{ asset('build/' . ltrim($jsAsset, '/')) }}"></script>
 @endif
