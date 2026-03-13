7380a4c8ae6538e2574b613abd5dfded23c2b5c0
nerry21 <nerrypopindo@gmail.com>
2026-03-13 16:29:28 +0700

    tambah luar biasa

--- resources/views/layouts/partials/vite-assets.blade.php
+++ resources/views/layouts/partials/vite-assets.blade.php
@@ -1,6 +1,14 @@
 @php
-    $viteManifestPath = public_path('build/manifest.json');
-    $viteManifest = file_exists($viteManifestPath)
+    $viteManifestCandidates = [
+        public_path('build/manifest.json'),
+        base_path('../build/manifest.json'),
+        base_path('build/manifest.json'),
+    ];
+
+    $viteManifestPath = collect($viteManifestCandidates)
+        ->first(fn (string $path): bool => file_exists($path));
+
+    $viteManifest = is_string($viteManifestPath) && $viteManifestPath !== ''
         ? json_decode(file_get_contents($viteManifestPath), true)
         : null;
 
@@ -13,10 +21,14 @@
         : null;
 @endphp
 
-@if (is_string($cssAsset) && $cssAsset !== '')
-    <link rel="stylesheet" href="{{ asset('build/' . ltrim($cssAsset, '/')) }}">
-@endif
+@if (file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+    @if (is_string($cssAsset) && $cssAsset !== '')
+        <link rel="stylesheet" href="{{ asset('build/' . ltrim($cssAsset, '/')) }}">
+    @endif
 
-@if (is_string($jsAsset) && $jsAsset !== '')
-    <script type="module" src="{{ asset('build/' . ltrim($jsAsset, '/')) }}"></script>
+    @if (is_string($jsAsset) && $jsAsset !== '')
+        <script type="module" src="{{ asset('build/' . ltrim($jsAsset, '/')) }}"></script>
+    @endif
 @endif
